3b5d6d12342fa2e118bed4b7341d54e9b22eeb95
FraneKuzmanic <frane.kuzmanic9@gmail.com>
2024-04-16 23:41:57 +0200

    Add project files.

---
+++ Prometej_core/Models/efModels/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prometej_core.Models.efModels
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ Prometej_persistance/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Prometej_core.Models.efModels;
+
+namespace Prometej_persistance
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<User> Users { get; set; }
+    }
+}
